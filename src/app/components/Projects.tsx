fc69103f92ce59d2c8953372cb6ea81a3c13425b
aryanraj07 <aryanraj010010@gmail.com>
2026-01-04 23:08:59 +0530

    made changes

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -55,11 +55,12 @@ const Projects = () => {
             >
               <div className="relative overflow-hidden aspect-video">
                 <Image
-                  className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110 object-bottom"
+                  className="object-cover object-bottom transition-transform duration-700 group-hover:scale-110"
                   src={item.img}
                   alt={item.title}
-                  width={200}
-                  height={200}
+                  fill
+                  sizes="(max-width: 768px) 100vw, 50vw"
+                  priority={false}
                 />
                 <div className="absolute inset-0 bg-bg-gradient-to-t from-card via-card/50 to-transparent opacity-60 " />
                 <div className="absolute inset-0 flex items-center justify-center gap-4 opacity-0 group-hover:opacity-100 transition-opacity dura300">
